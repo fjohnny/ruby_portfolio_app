04b8cdb7fe206b3388629e8936bfd2da540b462f
Johnny Franjo <johnnyfra@mail.com>
2018-05-03 22:31:39 +1000

    Added destroy action for portfolio feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -41,6 +41,15 @@ class PortfoliosController < ApplicationController
     
     end
 
+    def destroy
+        @portfolio_item = Portfolio.find(params[:id])
+        @portfolio_item.destroy
+
+        respond_to do |format|
+            format.html { redirect_to portfolios_path, notice: 'Portfolio Item was removed.' }
+        end
+    end
+
   # Never trust parameters from the scary internet, only allow the white list through.
   def portfolio_params
     params.require(:portfolio).permit(:title, :subtitle, :body)
